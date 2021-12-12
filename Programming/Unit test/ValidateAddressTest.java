35f454199a401c1b2309db3bb7524ec081a05ac2
duyentran123456 <duyen.ttt183910@sis.hust.edu.vn>
2021-12-12 17:46:08 +0700

    lab04

---
+++ Programming/Unit test/ValidateAddressTest.java
@@ -0,0 +1,30 @@
+package controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class ValidateAddressTest {
+
+	private PlaceOrderController placeOrderController;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		placeOrderController = new PlaceOrderController();
+	}
+	
+	@ParameterizedTest
+	@CsvSource({
+		"ha noi, true",
+		"so 15 ha noi, true",
+		"@#Fhanoi, false",
+		", false"
+	})
+	void test(String address, boolean expected) {
+		boolean isValid = placeOrderController.validateAddress(address);
+		assertEquals(expected, isValid);
+	}
+
+}
