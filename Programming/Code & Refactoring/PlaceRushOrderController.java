11ac4be1e98e78ef0b5079182b26efcac1ff07d8
Trần Thị Thanh Duyên <72453927+duyentran123456@users.noreply.github.com>
2021-12-12 20:16:09 +0700

    chore: format code in PlaceRushOrderControlle

--- Programming/Code & Refactoring/PlaceRushOrderController.java
+++ Programming/Code & Refactoring/PlaceRushOrderController.java
@@ -57,9 +57,9 @@ public class PlaceRushOrderController {
 	 */
 	public int calculateShippingFee(Order order) {
 		Random rand = new Random();
-        int fees = (int)( ( (rand.nextFloat()*10)/100 ) * order.getAmount() );
-        LOGGER.info("Order Amount: " + order.getAmount() + " -- Shipping Fees: " + fees);
-        return fees;
+		int fees = (int)( ( (rand.nextFloat()*10)/100 ) * order.getAmount() );
+		LOGGER.info("Order Amount: " + order.getAmount() + " -- Shipping Fees: " + fees);
+		return fees;
 	}
 	
 }
